072659995a17adda867663c6d3be3bbcf7a13f78
hex2w <72889112+hex2w@users.noreply.github.com>
2023-11-15 19:04:58 +0100

    v1.0.0-alpha

--- client/src/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    version: "1.0.0-beta",
-    authTokenEndpoint: "https://api.amazon.com/auth/o2/token",
-}
--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,1 +1,1 @@
-redocly build-docs ../api/sp-api.yaml -o ../api/docs/index.html
+redocly build-docs ../api/sp-api.yaml -o ../api/docs/index.html --config ../api/redocly-joined.yaml
